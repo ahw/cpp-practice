b260835f6bfa69df127a9c1ce546e36f3a2ff5f0
ahw <andrewhallagan@gmail.com>
2019-11-25 20:04:15 -0500

    Formatting 2

--- references.cpp
+++ references.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 class Foo {
-    public: 
+    public:
         int i;
 };
 
@@ -17,14 +17,14 @@ int main() {
     Foo* p1 = new Foo;
     Foo* p2 = p1;
 
-    std::cout << "*p1 \"toString\": " << *p1 << "\n";  // *p1 "toString": F(0)                 
-    std::cout << "*p1 \"toString\": " << *p2 << "\n";  // *p1 "toString": F(0)                 
-    std::cout << "a \"toString\": " << a << "\n";      // a "toString": F(460795941)           
-    std::cout << "b \"toString\": " << b << "\n";      // b "toString": F(-508596144)          
-    printf("Pointer address of  p1 %p\n", p1);         // Pointer address of  p1 0x7f8291401a10
-    printf("Pointer address of  p2 %p\n", p2);         // Pointer address of  p2 0x7f8291401a10
-    printf("Pointer address of &p1 %p\n", &p1);        // Pointer address of &p1 0x7ffee1af7028
-    printf("Pointer address of &p2 %p\n", &p2);        // Pointer address of &p2 0x7ffee1af7020
-    printf("Pointer address of  &a %p\n", &a);         // Pointer address of  &a 0x7ffee1af7038 
-    printf("Pointer address of  &b %p\n", &b);         // Pointer address of  &b 0x7ffee1af7030 
+    std::cout << "*p1 \"toString\": " << *p1 << "\n"; // F(0)
+    std::cout << "*p1 \"toString\": " << *p2 << "\n"; // F(0)
+    std::cout << "a \"toString\": " << a << "\n"; // F(460795941)
+    std::cout << "b \"toString\": " << b << "\n"; // F(-508596144)
+    printf("Pointer address of  p1 %p\n", p1); // 0x7f8291401a10
+    printf("Pointer address of  p2 %p\n", p2); // 0x7f8291401a10
+    printf("Pointer address of &p1 %p\n", &p1); // 0x7ffee1af7028
+    printf("Pointer address of &p2 %p\n", &p2); // 0x7ffee1af7020
+    printf("Pointer address of  &a %p\n", &a); // 0x7ffee1af7038
+    printf("Pointer address of  &b %p\n", &b); // 0x7ffee1af7030
 }
